fb176e1c9f093ac94bd3b8563514d8156c400171
Ithalos <sam.triest@gmail.com>
2015-11-21 02:27:35 +0100

    Added some back-end support for the pause menu.

---
+++ FishingGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour 
+{
+    public GameObject pauseUI;
+    bool gameIsPaused;
+
+    void Start()
+    {
+        // At the start of the game, the pause UI will be disabled and the game will run.
+        pauseUI.SetActive(false);
+        gameIsPaused = false;
+    }
+
+
+    void Update()
+    {
+
+        // Reverse pause state.
+        if (Input.GetButtonDown("Pause"))
+        {
+            gameIsPaused = !gameIsPaused;
+        }
+
+
+        // Show the pause menu & stop time.
+        if (gameIsPaused)
+        {
+            pauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        // Hide the pause menu & continue time.
+        if (!gameIsPaused)
+        {
+            pauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+
+    // Resume the game.
+    public void Resume()
+    {
+        gameIsPaused = false;
+    }
+
+    // Reload the current level, thus resetting or restarting the level.
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    // Load the level called "MainMenu". This is the main menu. Hard code is hard.
+    // Comment this out when MainMenu has actually been created.
+    public void MainMenu()
+    {
+        // Application.LoadLevel("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
