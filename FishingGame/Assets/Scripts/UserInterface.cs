5970214d33f24ee17f86dfb7870666d5e3cd5bf4
Ithalos <sam.triest@gmail.com>
2015-11-22 02:33:32 +0100

    Added back-end support for opening and closing inventory UI.

---
+++ FishingGame/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserInterface : MonoBehaviour 
+{
+    public GameObject inventoryUI;
+    bool inventoryOpen;
+
+	void Start () 
+    {
+        inventoryOpen = false;
+        inventoryUI.SetActive(false);
+	}
+	
+
+	void Update () 
+    {
+        if (Input.GetButtonDown("Inventory"))
+        {
+            inventoryOpen = !inventoryOpen;
+        }
+
+        if (inventoryOpen)
+        {
+            inventoryUI.SetActive(true);
+        }
+        else
+        {
+            inventoryUI.SetActive(false);
+        }
+	}
+
+}
